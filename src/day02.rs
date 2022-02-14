43d5f854626e5fa0e135b110e3fe78ee4cce48f4
sstollenwerk <sstollenwerk@yahoo.co.nz>
2022-02-15 10:28:45 +1300

    Do day 02 part 2

--- src/day02.rs
+++ src/day02.rs
@@ -4,11 +4,12 @@ use std::collections::HashSet;
 use std::fs;
 
 use counter::Counter;
+use itertools::Itertools;
 
 type Num = u32;
 
 fn get_data() -> Vec<String> {
-    fs::read_to_string(to_filename(02))
+    fs::read_to_string(to_filename(2))
         .expect("Could not read file")
         .lines()
         .map(|s| s.to_string())
@@ -28,14 +29,31 @@ fn appears(n: Num, vals: &String) -> bool {
     counts(&vals).contains(&n)
 }
 
+fn similarity(a: &String, b: &String) -> Option<String> {
+    let similars: String = a
+        .chars()
+        .zip(b.chars())
+        .filter(|(x, y)| x == y)
+        .map(|(x, y)| x)
+        .collect();
+    if similars.len() + 1 == a.len() {
+        Some(similars)
+    } else {
+        None
+    }
+}
+
 pub fn part1() -> usize {
     let vals = get_data();
 
-    vals.iter().filter( |x| appears(2, &x)  ).count() * vals.iter().filter( |x| appears(3, &x)  ).count()
-
-    //todo!();
+    vals.iter().filter(|x| appears(2, &x)).count() * vals.iter().filter(|x| appears(3, &x)).count()
 }
 
-pub fn part2() -> Num {
-    todo!();
+pub fn part2() -> String {
+    let vals = get_data();
+    vals.iter()
+        .combinations(2)
+        .filter_map(|g| similarity(g[0], g[1]))
+        .next()
+        .unwrap()
 }
