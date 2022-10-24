4f82fd91040d619da359a81640f2a94d9e037007
sstollenwerk <sstollenwerk@yahoo.co.nz>
2022-10-25 08:51:38 +1300

    Do day12 part2

--- src/day12.rs
+++ src/day12.rs
@@ -4,7 +4,9 @@ use std::collections::HashSet;
 use itertools::Itertools;
 use itertools::MinMaxResult::{MinMax, NoElements, OneElement};
 
-type State = HashSet<i32>;
+type Num = i64;
+
+type State = HashSet<Num>;
 type Map = HashMap<Vec<bool>, bool>;
 
 fn exists(c: char) -> bool {
@@ -21,7 +23,7 @@ fn parse(s: &str) -> (State, Map) {
     let state: State = as_row(parts.next().unwrap().split(' ').nth(2).unwrap())
         .into_iter()
         .enumerate()
-        .filter_map(|(i, el)| if el { Some(i as i32) } else { None })
+        .filter_map(|(i, el)| if el { Some(i as Num) } else { None })
         .collect();
     parts.next();
 
@@ -55,15 +57,57 @@ fn step(trans: &Map, state: &State) -> State {
         .collect()
 }
 
-pub fn part1(s: &str) -> i32 {
+pub fn part1(s: &str) -> Num {
     let (mut state, trans) = parse(s);
     for _ in 0..20 {
-        state = step(&trans, &state)
+        state = step(&trans, &state);
     }
 
     state.into_iter().sum()
 }
 
-pub fn part2(s: &str) -> i32 {
-    todo!();
+fn as_adj(state: &State) -> Vec<Num> {
+    let k = state.iter().min().unwrap();
+    state.iter().map(|c| c - k).sorted().collect()
+}
+
+fn score(adj: &Vec<Num>, t: Num) -> Num {
+    assert!(adj[0] == 0);
+    adj.iter().sum::<Num>() + t * (adj.len() as Num)
+}
+
+pub fn part2(s: &str) -> Num {
+    let (mut state, trans) = parse(s);
+
+    let mut seen: HashMap<Vec<Num>, (Num, usize)> = HashMap::new();
+    let steps = 50000000000;
+    for i in 0..steps {
+        state = step(&trans, &state);
+
+        let k = state.iter().min().unwrap();
+        let adj: Vec<Num> = as_adj(&state);
+
+        assert!(state.iter().sum::<Num>() == score(&adj, *k));
+
+        let current = (*k, i);
+
+        if let Some(prev) = seen.insert(adj.clone(), current) {
+            dbg!(&current);
+            dbg!(&prev);
+
+            let delta = current.0 - prev.0;
+            let time = current.1 - prev.1;
+
+            let remaining = steps - (i + 1);
+
+            assert!(remaining % time == 0);
+
+            let shift = k + (delta * (remaining / time) as Num);
+            return score(&adj, shift);
+        }
+    }
+
+    unreachable!();
+
+    //state.into_iter().sum()
 }
