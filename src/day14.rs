9b6c682e81bdd4c126fa6f64302effc81b4a12fe
sstollenwerk <sstollenwerk@yahoo.co.nz>
2022-10-30 12:07:31 +1300

    Do day14 part2 Would have been better to use iterators instead of duplicated code

--- src/day14.rs
+++ src/day14.rs
@@ -33,6 +33,34 @@ fn recipes(amt: Num) -> Num {
     parse(&show)
 }
 
+fn recipes2(desired: &Vec<Num>) -> Num {
+    let mut a: usize = 0;
+    let mut b: usize = 1;
+
+    let mut vals = vec![3, 7];
+
+
+    loop {
+        let part = digits(vals[a] + vals[b]);
+        let amt = part.len();
+        vals.extend(part);
+
+        a += vals[a] + 1;
+        b += vals[b] + 1;
+        a %= vals.len();
+        b %= vals.len();
+
+        if vals.len() >= amt + desired.len() {
+            for i in (1..=amt) {
+                let start = vals.len() - i - desired.len();
+                if vals[start..start + desired.len()] == desired[..] {
+                    return start;
+                }
+            }
+        }
+    }
+}
+
 pub fn part1(s: &str) -> Num {
     let amt = parse(s);
 
@@ -40,5 +68,7 @@ pub fn part1(s: &str) -> Num {
 }
 
 pub fn part2(s: &str) -> Num {
-    todo!();
+    let target = digits(parse(s));
+
+    recipes2(&target)
 }
